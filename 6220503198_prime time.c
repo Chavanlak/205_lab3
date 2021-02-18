ddc485bbc989673c56c29c401b1e94d95913492c
Chavanlak <chavanlak.p@ku.th>
2021-02-18 16:41:02 +0700

    Update 6220503198_prime time.c
    รับค่าช่วงเวลา p กับ a

--- 6220503198_prime time.c
+++ 6220503198_prime time.c
@@ -2,7 +2,24 @@
 int main()
 {
     int i,n,time[48]={0},prime,sum=0;
-    55555555555
+    int a,p;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+       scanf("%d%d",&p,&a)
+
+    }
+    for()
+    {
+        if()
+        {
+
+        }
+    }
+
+
+
+    return 0;
 
 
 }
