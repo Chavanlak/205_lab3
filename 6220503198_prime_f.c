7bf1cdec05b95dd36d6c5be5c120da4220bd40a8
Chavanlak <chavanlak.p@ku.th>
2021-02-18 17:39:27 +0700

    Update 6220503198_prime_f.c
    int main

--- 6220503198_prime_f.c
+++ 6220503198_prime_f.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
-int main()
-{
-    int x,p,n;
-    scanf("%d",&x);
-    for()
-    {
 
+    int function(int x)
+    {
+        int i;
+    for(i=2;i<x;i++)
+    {
+        if(x%i==0)
+        {
+            return 0;
+        }
 
     }
 
 }
+int main()
+{
+    int x,min,max,i;
+}
+
